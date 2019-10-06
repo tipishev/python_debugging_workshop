7c86ffd170d122f5d522e2f4cb59e21b4e840087
Timofey Tipishev <tipishev@gmail.com>
2019-10-06 20:10:31 +0200

    add trappy lvl

---
+++ dungeon/levels/trappy.py
@@ -0,0 +1,12 @@
+def traps_corridor(player):
+    # watch out, many traps to avoid
+
+    print(42)
+    pass
+    raise Exception('smashed by a rock')
+    pass
+    raise Exception('smashed by a rock')
+    player.inventory.append('traps-key')
+    return player
+
+
