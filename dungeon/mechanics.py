0613a3e2a86cbec4c37aaba3358ab186fc03e4e3
Timofey Tipishev <tipishev@gmail.com>
2019-10-07 23:37:23 +0200

    remove enter global method in favor of keys

--- dungeon/mechanics.py
+++ dungeon/mechanics.py
@@ -1,7 +1,3 @@
-def enter(player, location):
-    if player.dares(location):
-        return location(player)
-
 def interact(*actors):
     '''
     give every actor a list of other actors and collect actor's action
