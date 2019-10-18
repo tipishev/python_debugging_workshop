06b08344392d163230d18efe091f500a52a24f97
Timofey Tipishev <tipishev@gmail.com>
2019-10-18 21:42:06 +0200

    add some looking pointers

--- dungeon/levels/looking.py
+++ dungeon/levels/looking.py
@@ -5,9 +5,134 @@ def looking_corridor(player):
     breakpoint()
     _  # there are a few ways to look at the source in PDB
     _  # type `list`
-    _
     _  # by default it shows you 11 lines
-    _  # current line, 5 above, and 5 below
+    _  # 5 above, current line, and 5 below
+    _
+    _  # abbreviate the command to `l` to see the next 11 lines
+    _
+    _
+    _
+    _  # press `Enter` to see the next 11 lines
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _  # press `Enter` again...
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _  # you can also specify the range
+    _  # with (l)ist <first line number>,<list line number>
+    _  # try `l 50 55`
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _  # awesome!
+    _
+    _ # if you specify  
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
+    _
     _
     _
     _
