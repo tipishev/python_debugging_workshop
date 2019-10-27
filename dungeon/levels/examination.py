90d97287e9a618d6523861a6fb29c12f9ca0d62b
Timofey Tipishev <tipishev@gmail.com>
2019-10-27 19:06:47 +0100

    add bag of dicts to the corridor of examination

---
+++ dungeon/levels/examination.py
@@ -0,0 +1,21 @@
+bag_of_dicts = [
+    {
+        'one': 'in',
+        'this': 'bag',
+        'distractions': 'finds',
+        'though': '...'
+    },
+    {
+        'desires': {
+            'have': 'a',
+            'Amulet': 'shall',
+        },
+        'a': {
+            'someone': 'who',
+        },
+    },
+    {
+        'zebra': 'in',
+    },
+    {'own': 'bag'},
+]
